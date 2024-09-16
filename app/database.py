21574b22cc31579aaf13501e13231e8cc024d2de
hmcdl <mihalevch@mail.ru>
2024-09-16 17:13:07 +0300

    new structure for aforms web

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.secret_data import DATABASE_PASSWORD
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://postgres:{DATABASE_PASSWORD}@localhost:5432/simulations_db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    echo=True
+)
+
+Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/users/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship, mapped_column
+
+from app.database import Base
+from ..simulations.models import Simulation
+
+class User(Base):
+    """
+    Класс, представляющий сущность пользователя для орм-ки
+    """
+    __tablename__ = "users"
+    id = mapped_column(Integer, primary_key=True)
+    email = mapped_column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    role_id = Column(Integer)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+    availible_simulations = Column(Integer, default=100000, nullable=False)
+
+    simulations = relationship("Simulation", back_populates="owner")
+    
